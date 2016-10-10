1d4f94d77388830ca2c97cf90ce5f38887f6c629
xiqingliu <546056884@qq.com>
2016-10-10 11:25:48 +0800

    高德离线地图Demo

--- recyclerbaseadapter/src/main/java/com/alenbeyond/recyclerbaseadapter/MainActivity.java
+++ recyclerbaseadapter/src/main/java/com/alenbeyond/recyclerbaseadapter/MainActivity.java
@@ -2,8 +2,12 @@ package com.alenbeyond.recyclerbaseadapter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
