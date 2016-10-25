53caea689eb7e7cebb2c86df7b8aeebdb1ccfc27
xiqingliu <546056884@qq.com>
2016-10-25 19:45:42 +0800

    高德夜间模式

---
+++ zuji/src/main/java/com/alenbeyond/zuji/MainActivity.java
@@ -0,0 +1,47 @@
+package com.alenbeyond.zuji;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+    private AMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMapView = (MapView) findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+        if (map == null) {
+            map = mMapView.getMap();
+        }
+        map.setMapType(AMap.MAP_TYPE_NIGHT);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+}
